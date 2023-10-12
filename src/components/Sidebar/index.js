08d03a37a2a40bc9735ee630fccb2e53eeb54b2f
Bifabig <biftubig@gmail.com>
2023-10-12 17:25:46 +0300

    add sidebar component

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -2,6 +2,13 @@ import { Link, NavLink } from 'react-router-dom';
 import './index.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+import {
+  faAngellist,
+  faGithub,
+  faLinkedin,
+  faMedium,
+  faTwitter,
+} from '@fortawesome/free-brands-svg-icons';
 
 import LogoS from '../../assets/images/logo-s.png';
 import LogoSubtitle from '../../assets/images/logo_sub.png';
@@ -12,17 +19,52 @@ const Sidebar = () => (
       <img src={LogoS} alt="logo" />
       <img className="sub-logo" src={LogoSubtitle} alt="subtitle" />
     </Link>
-    <nav>
+    <nav id="sidebar">
       <NavLink exact="true" to="/">
         <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
       </NavLink>
-      <NavLink exact="true" to="/about">
+      <NavLink exact="true" className="about-link" to="/about">
         <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
       </NavLink>
       <NavLink exact="true" className="contact-link" to="/contact">
         <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
       </NavLink>
     </nav>
+    <ul>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.linkedin.com/in/biftu-girma"
+        >
+          <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a target="_blank" rel="noreferrer" href="https://github.com/Bifabig">
+          <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a target="_blank" rel="noreferrer" href="https://twitter.com/biftu94">
+          <FontAwesomeIcon icon={faTwitter} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a target="_blank" rel="noreferrer" href="https://medium.com/@biftubig">
+          <FontAwesomeIcon icon={faMedium} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://wellfound.com/u/biftu-girma"
+        >
+          <FontAwesomeIcon icon={faAngellist} color="#4d4d4e" />
+        </a>
+      </li>
+    </ul>
   </div>
 );
 
